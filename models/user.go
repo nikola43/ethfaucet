2dec58f8c51d4c8236981a10429b4fe0df91a797
Paulo <pauloxti@gmail.com>
2023-03-20 22:17:47 +0100

    ip

--- models/user.go
+++ models/user.go
@@ -9,6 +9,6 @@ import (
 type User struct {
 	base.CustomGormModel
 	WalletAddress string    `gorm:"index; unique; type:varchar(64) not null" json:"wallet_address"`
-	IPAddress     string    `gorm:"index; unique; type:varchar(11) not null" json:"ip_address"`
+	IPAddress     string    `gorm:"index; unique; type:varchar(24) not null" json:"ip_address"`
 	ClaimDate     time.Time `json:"claim_date"`
 }
